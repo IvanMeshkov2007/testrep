fe01ce16f81f9636b97e07068af08356ee7a8c37
IvanMeshkov2007 <Ivashka270507@gmail.com>
2024-08-12 13:16:09 +0300

    first commit

---
+++ HM_2_5.cpp
@@ -0,0 +1,26 @@
+﻿// HM_2_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>;
+#include <map>;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    map <string, int>  products{
+    {"bread", 1}, {"milk", 2}, {"apple", 3},{"orange",4},{"tea",5}
+    };
+    for (auto elem : products) {
+        cout << elem.first <<" " << elem.second << "\n";
+    }
+    cout << "\n";
+    products.insert({"water", 7 });
+    products.insert({"coffe", 6 });
+    for (auto elem : products) {
+        cout << elem.first << " " << elem.second << "\n";
+    }
+
+}
+
